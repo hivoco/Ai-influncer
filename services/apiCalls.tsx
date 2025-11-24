b73e2c44468bc7e1998005d8314dc30c1b838342
ranjanhivoco <ranjan@hivoco.com>
2025-11-24 21:20:04 +0530

    api base change

--- services/apiCalls.tsx
+++ services/apiCalls.tsx
@@ -2,7 +2,7 @@ export const API_BASE = "https://next.hivoco.com";
 
 // "https://backend.thefirstimpression.ai";
 // "http://192.168.1.11:8000";
-("http://localhost:8000");
+// ("http://localhost:8000");
 
 export const getCampaignPrompts = async (id: string) => {
   const data = await fetch(API_BASE + `/prompts?brand_id=${id}`, {
